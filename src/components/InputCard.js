8925077371c90ffa064366a63951f1550d29539c
Asha <maymunahrose@gmail.com>
2022-08-17 13:57:44 -0400

    Added regex logic and color derivation logic

--- src/components/InputCard.js
+++ src/components/InputCard.js
@@ -1,27 +1,58 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import GenerateBtn from "./GenerateBtn";
 
-const InputCard = (props) =>{
-    const [userInput, setUserInput] = useState(''); 
-    const ref = useRef(); 
-    const handleUserInput = (e) =>{
-        e.preventDefault(); 
-        setUserInput(ref.current.value)
+const cssColorNames = /(AliceBlue|AntiqueWhite|Aqua|Aquamarine|Azure|Beige|Bisque|Black|BlanchedAlmond|Blue|BlueViolet|Brown|BurlyWood|CadetBlue|Chartreuse|Chocolate|Coral|CornflowerBlue|Cornsilk|Crimson|Cyan|DarkBlue|DarkCyan|DarkGoldenRod|DarkGray|DarkGrey|DarkGreen|DarkKhaki|DarkMagenta|DarkOliveGreen|DarkOrange|DarkOrchid|DarkRed|DarkSalmon|DarkSeaGreen|DarkSlateBlue|DarkSlateGray|DarkSlateGrey|DarkTurquoise|DarkViolet|DeepPink|DeepSkyBlue|DimGray|DimGrey|DodgerBlue|FireBrick|FloralWhite|ForestGreen|Fuchsia|Gainsboro|GhostWhite|Gold|GoldenRod|Gray|Grey|Green|GreenYellow|HoneyDew|HotPink|IndianRed|Indigo|Ivory|Khaki|Lavender|LavenderBlush|LawnGreen|LemonChiffon|LightBlue|LightCoral|LightCyan|LightGoldenRodYellow|LightGray|LightGrey|LightGreen|LightPink|LightSalmon|LightSeaGreen|LightSkyBlue|LightSlateGray|LightSlateGrey|LightSteelBlue|LightYellow|Lime|LimeGreen|Linen|Magenta|Maroon|MediumAquaMarine|MediumBlue|MediumOrchid|MediumPurple|MediumSeaGreen|MediumSlateBlue|MediumSpringGreen|MediumTurquoise|MediumVioletRed|MidnightBlue|MintCream|MistyRose|Moccasin|NavajoWhite|Navy|OldLace|Olive|OliveDrab|Orange|OrangeRed|Orchid|PaleGoldenRod|PaleGreen|PaleTurquoise|PaleVioletRed|PapayaWhip|PeachPuff|Peru|Pink|Plum|PowderBlue|Purple|RebeccaPurple|Red|RosyBrown|RoyalBlue|SaddleBrown|Salmon|SandyBrown|SeaGreen|SeaShell|Sienna|Silver|SkyBlue|SlateBlue|SlateGray|SlateGrey|Snow|SpringGreen|SteelBlue|Tan|Teal|Thistle|Tomato|Turquoise|Violet|Wheat|White|WhiteSmoke|Yellow|YellowGreen)/gim;
+
+const hslOrRgb = /(hsla?\(|rgba?\()[a-zA-z0-9%,.\s]+\)/g;
+const hex = /#[\w|\d]{1,8}/g;
+
+const InputCard = (props) => {
+  const [userInput, setUserInput] = useState("");
+  const [userColors, setUserColors] = useState([]);
+
+  const ref = useRef();
+
+  const handleUserInput = (e) => {
+    e.preventDefault();
+    setUserInput(ref.current.value);
+  };
+
+  useEffect(() => {
+    const hslOrRgbArray = userInput.match(hslOrRgb);
+    const hexArray = userInput.match(hex);
+    const colorNameArray = userInput.match(cssColorNames);
+
+    let allColors = [];
+    if (hslOrRgbArray) {
+      allColors = [...allColors, ...hslOrRgbArray];
     }
 
-    console.log(userInput.match(regex));  
+    if (hexArray) {
+      allColors = [...allColors, ...hexArray];
+    }
 
-    return(
-        <div>
-            <p>Insert your color codes here:</p>
-            <div>
-                <form>
-                    <textarea rows="15" cols="60" ref={ref}/>
-                    <GenerateBtn onClick={handleUserInput}/>
-                </form>
-            </div>
-        </div>
-    )
-}
+    if (colorNameArray) {
+      allColors = [...allColors, ...colorNameArray];
+    }
 
-export default InputCard; 
+    if(allColors){
+        setUserColors(allColors)
+    }
+  }, [userInput]);
+
+  console.log(userColors);
+
+  return (
+    <div>
+      <p>Insert your color codes here:</p>
+      <div>
+        <form>
+          <textarea rows="15" cols="60" ref={ref} />
+          <GenerateBtn onClick={handleUserInput} />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default InputCard;
