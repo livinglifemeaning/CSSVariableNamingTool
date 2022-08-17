51fbc2b7b4fa5614254e8df25844f1fe853f62f4
Asha <maymunahrose@gmail.com>
2022-08-17 16:15:58 -0400

    Added color conversion for rgb and hex colors into hsl

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## Useful Resources
 
 - https://regex101.com/ , https://www.youtube.com/watch?v=sa-TUpSx1JA (learned regular expressions)
+
 - https://www.youtube.com/watch?v=RLWniwmfdq4 (how to forward refs)
+
+-https://www.npmjs.com/package/color-convert
--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,14 +1,58 @@
+import { useEffect } from "react";
 import CopyBtn from "./CopyBtn";
 
-const ResultCard = (props) => {
+const ResultCard = ({ colors }) => {
+  const convert = require("color-convert");
 
+  const hslColors = colors.filter((color) => color.startsWith("hsl"));
+  const rgbColors = colors.filter((color) => color.startsWith("rgb"));
+  const hexColors = colors.filter((color) => color.startsWith("#"));
+
+  let convertedColors = [...hslColors];
+
+  if (rgbColors.length) {
+    rgbColors.map((color) => {
+      let rgbNumber = color.replaceAll(/[^0-9,]/gi, "");
+      let convertedColor = convert.rgb.hsl(rgbNumber);
+      let endConversion = `hsl(${convertedColor[0]}, ${convertedColor[1]}%, ${convertedColor[2]}%)`;
+      return (convertedColors = [...convertedColors, endConversion]);
+    });
+  }
+
+  if (hexColors.length) {
+    hexColors.map((color) => {
+      let hexValue = color.replaceAll(/[^#]/gi, "");
+      let convertedColor = convert.hex.hsl(hexValue);
+      let endConversion = `hsl(${convertedColor[0]}, ${convertedColor[1]}%, ${convertedColor[2]}%)`;
+      return (convertedColors = [...convertedColors, endConversion]);
+    });
+  }
+  console.log(convertedColors);
+  //   const convertRgbColors = (color) => {
+
+  //   }
+
+  //   let convertedColor = convert.rgb.hsl(255, 0, 0)
+  //   let end =
+
+  //   console.log(end);
+  //   console.log(hslColors, rgbColors, hexColors);
 
   return (
     <div>
       <p>Result:</p>
       <div>
-        
-        <p>{props.colors.join(' ')}</p>
+        <p styles={{ height: "500px" }}>
+          --root<br/>
+          {convertedColors.map((color) => {
+            return (
+              <>
+                <span>{color};</span>
+                <br />
+              </>
+            );
+          })}
+        </p>
         <CopyBtn />
       </div>
     </div>
